473df9e4540a056d8c62029d4e0d85ddbcaa84d9
Gorakh Joshi <gorakhjoshi@192.168.1.20>
2022-05-03 13:35:30 +0545

    react version downgraded to 17.0.2

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
 import "./index.css";
 import App from "./containers/App/App";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
